7fb8a2c49cd9bd15833320260d0be0b4d92ec9c9
Aziz Hidri <hidriaziz643@gmail.com>
2026-05-01 23:13:05 +0200

    Change window title for library management system

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from controller import librarycontroller
 
 if __name__=="__main__":
     root = Tk()
-    root.title("Mouheb_Takwa_Aziz")
+    root.title("Library_Management_System")
     root.geometry("1100x600")
     model=library()
     view=librarywindow(root,model)
